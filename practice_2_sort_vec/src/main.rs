0f9168b760c61730770a2a066b26db43a5ec7c23
Gris Ge <fge@redhat.com>
2025-10-23 19:52:41 +0800

    Make the practice slightly hard
    Signed-off-by: Gris Ge <fge@redhat.com>

--- practice_2_sort_vec/src/main.rs
+++ practice_2_sort_vec/src/main.rs
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: Apache-2.0
 
 fn main() {
-    let mut data = vec![9u8, 1, 7, 2, 8];
-    data.sort_unstable();
+    let mut data = Some(vec![9u8, 1, 7, 2, 8]);
+    data.as_mut().map(|d| d.sort_unstable());
     println!("{:?}", data);
 }
