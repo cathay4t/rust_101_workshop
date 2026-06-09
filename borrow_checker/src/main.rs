2e212e133e61e43cac79e12cbd88226006f1f7ea
Gris Ge <cnfourt@gmail.com>
2026-06-09 10:17:49 +0800

    Add notes for borrow checker
    Signed-off-by: Gris Ge <cnfourt@gmail.com>

--- borrow_checker/src/main.rs
+++ borrow_checker/src/main.rs
@@ -1,8 +1,14 @@
 // SPDX-License-Identifier: Apache-2.0
 
 fn main() {
-    let mut a = vec![1u8, 2, 3, 4];
+    let org = vec![1u8,2,3,4];
+    let mut a = org;
+
+
     let item0 = &a[0];
+
+    // uncommon this line will cause compile failure
+    // a.push(5);
 
     println!("{}", item0);
 
